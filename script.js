821079eba5d3151efaa7b3a9b6135c0ae7eca64c
Jonathan Gomez <jongomezdev@gmail.com>
2020-12-02 11:45:05 -0500

    Declare variables

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ const questionEl = document.getElementById("question");
 const answerButtonsEl = document.getElementById("answer-buttons");
 const timer = document.querySelector("#time");
 const messageDiv = document.querySelector("#message");
+
+// **************** Declare Variables *******************
+let score = 0;
+let time = 60;
+let randQuestions = "";
+let currentQuestion = "";
+let soundFX = new Audio("");
