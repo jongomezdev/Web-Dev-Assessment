2ad3ca93e07548924a33fd74f8e6963b35d7220a
Jonathan Gomez <jongomezdev@gmail.com>
2020-12-02 11:40:16 -0500

    Grab DOM elements

---
+++ script.js
@@ -0,0 +1,7 @@
+// *************** Grab DOM Elements ****************
+const startButton = document.getElementById("start-btn");
+const questionContainer = document.getElementById("question-container");
+const questionEl = document.getElementById("question");
+const answerButtonsEl = document.getElementById("answer-buttons");
+const timer = document.querySelector("#time");
+const messageDiv = document.querySelector("#message");
