a22b2ef22215d66b43f71c0815ab10939cb6cd47
Jonathan Gomez <jongomezdev@gmail.com>
2020-12-02 22:41:03 -0500

    Rename classes and functions for less confusion

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // *************** Grab DOM Elements ****************
-const startButton = document.getElementById("start-btn");
-const questionContainer = document.getElementById("question-container");
-const questionEl = document.getElementById("questionText");
-const answerButtonsEl = document.getElementById("answer-buttons");
+const startBtn = document.getElementById("startButton");
+const questionText = document.getElementById("questionText");
+const gameContainer = document.getElementById("game-div");
+const answerButtons = document.getElementById("answers");
 const timer = document.querySelector("#time");
 const messageDiv = document.querySelector("#message");
 
@@ -10,7 +10,7 @@ const messageDiv = document.querySelector("#message");
 let score = 0;
 let time = 0;
 let randQuestion = "";
-let currentQuestion = "";
+let currentQuestionIndex = 0;
 let soundFX = new Audio("");
 
 startButton.addEventListener("click", startGame);
@@ -23,49 +23,65 @@ function setTime() {
 
     if (time === 0) {
       clearInterval(timerInterval);
-      alert("Out of Time");
-
-      if (currentQuestion < questions.length - 1) {
-        endGame();
-      }
+      alert("You ran out of time!");
+      // endGame();
+    } else if (currentQuestionIndex === questions.length) {
+      clearInterval(timerInterval);
     }
   }, 1000);
-  return score;
+  // return score;
 }
 
 // **************** Start Game *******************
 function startGame() {
-  startButton.classList.add("hide");
+  startBtn.classList.add("hide");
   randQuestion = questions.sort(() => Math.random() - 0.5);
-  currentQuestion = 0;
-  questionContainer.classList.remove("hide");
-  getNewQuestion();
+  currentQuestionIndex = 0;
+  showNextQuestion();
 }
 
 // **************** Show Question *******************
-function showQuestion() {
-  getNewQuestion();
+function showNextQuestion() {
+  showQuestion(randQuestion[currentQuestionIndex]);
 }
 
 // **************** Get New Question *******************
-function getNewQuestion() {
-  questionText.textContent = questions[currentQuestion].question;
+function showQuestion() {
+  if (currentQuestionIndex === questions.length) {
+    // endGame();
+  } else {
+    questionText.innerText = questions[currentQuestionIndex].question;
+    answers.textContent = questions[currentQuestionIndex].choices[0];
+    answers.textContent = questions[currentQuestionIndex].choices[1];
+    answers.textContent = questions[currentQuestionIndex].choices[2];
+    answers.textContent = questions[currentQuestionIndex].choices[3];
+  }
 }
+// answerButtons.addEventListener("click");
+
+// ************ Select Answer Function *****************
+function selectAnswer() {}
 
 // **************** Reset States *******************
 function resetQuestion() {}
 
 function endGame() {}
 
+// ************************* Questions ***********************
 const questions = [
   {
     question: "How do you create a function in JavaScript",
     choices: [
       "function myFunction()",
-      "function myFunction()",
-      "function myFunction()",
-      "function myFunction()",
+      "function = myFunction()",
+      "function:myFnction()",
+      "function()",
     ],
     answer: "function myFunction()",
+  },
+  {
+    question: "Which event occurs when the user clicks on an HTML element?",
+    choices: ["onmouseclick", "onchange", "onclick", "onmouseover"],
+    answer: "onclick",
   },
 ];
