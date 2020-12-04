f3796d3fe04abb14cdc1a64df752caefceb8c85b
Jonathan Gomez <jongomezdev@gmail.com>
2020-12-03 23:31:48 -0500

    Create table in highscoresjs

--- highScores.js
+++ highScores.js
@@ -3,4 +3,36 @@ const highScoreList = document.getElementById("highScores");
 const clearScores = document.getElementById("clear-scores");
 
 // ************* show scores function *****************
-function showHighScores() {}
+function runScores() {
+  savedScores = JSON.parse(localStorage.getItem("scores"));
+  if (savedScores !== null) {
+    scoreList = savedScores;
+  }
+  showScores();
+}
+
+function showScores() {
+  if (savedScores !== null) {
+    scoreList.sort((a, b) => {
+      return a.newScore - b.newScore;
+    });
+    scoreList.reverse((a, b) => {
+      return a.newScore - b.newScore;
+    });
+    for (i = 0; i < scoreList.length; i++) {
+      let scoreListItem = scoreList[i];
+      let tr = document.createElement("tr");
+      let cellName = document.createElement("td");
+      let cellNameText = document.createTextNode(scoreListItem.name);
+      let cellScore = document.createElement("td");
+      let cellScoreNum = document.createTextNode(scoreListItem.newScore);
+
+      tr.setAttribute("tr-index", i);
+      document.getElementById("highScores").appendChild(tr);
+      tr.appendChild(cellName);
+      cellName.appendChild(cellNameText);
+      tr.appendChild(cellName);
+      cellScore.appendChild(cellScoreNum);
+    }
+  }
+}
